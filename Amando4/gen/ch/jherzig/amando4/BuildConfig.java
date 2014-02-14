074303199d873bdea0964df6668e981464cb0881
jherzig <info@jherzig.ch>
2014-02-14 18:29:42 +0100

    initial

---
+++ Amando4/gen/ch/jherzig/amando4/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ch.jherzig.amando4;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- Brutto_Netto_Rechner/gen/ch/jherzig/rechner/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ch.jherzig.rechner;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
