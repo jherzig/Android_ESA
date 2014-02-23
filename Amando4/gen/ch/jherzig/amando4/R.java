14d95b0ec3b0a67f6a854b29b13a334c40384439
jherzig <info@jherzig.ch>
2014-02-23 12:25:10 +0100

    Kapitel 5.9.4

--- Amando4/gen/ch/jherzig/amando4/R.java
+++ Amando4/gen/ch/jherzig/amando4/R.java
@@ -10,6 +10,11 @@ package ch.jherzig.amando4;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int hintergrund=0x7f040000;
+        public static final int schwarz=0x7f040001;
+        public static final int textfarbe=0x7f040002;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,52 +22,48 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int amando_logo=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int notification_icon=0x7f020003;
+        public static final int pfeil=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090000;
     }
     public static final class layout {
         public static final int startseite=0x7f030000;
     }
     public static final class menu {
-        public static final int startseite=0x7f070000;
+        public static final int startseite=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int men_amando_beenden=0x7f050006;
-        public static final int men_einstellungen_bearbeiten=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int men_amando_beenden=0x7f060006;
+        public static final int men_einstellungen_bearbeiten=0x7f060005;
         /**  Optionsmenue 
          */
-        public static final int men_hilfe_anzeige=0x7f050004;
-        public static final int sf_geokontakte_verwalten=0x7f050009;
-        public static final int sf_karte_anzeigen=0x7f05000a;
+        public static final int men_hilfe_anzeige=0x7f060004;
+        public static final int sf_geokontakte_verwalten=0x7f060009;
+        public static final int sf_karte_anzeigen=0x7f06000a;
         /**  Schaltflächentext 
          */
-        public static final int sf_position_senden=0x7f050007;
-        public static final int sf_position_senden_beenden=0x7f050008;
-        public static final int sf_simultation=0x7f05000b;
-        public static final int startseite_titel=0x7f050003;
+        public static final int sf_position_senden=0x7f060007;
+        public static final int sf_position_senden_beenden=0x7f060008;
+        public static final int sf_simultation=0x7f06000b;
+        public static final int startseite_titel=0x7f060003;
         /**  Startseite 
          */
-        public static final int txt_startseiteanzeigen_intro=0x7f050002;
+        public static final int txt_startseiteanzeigen_intro=0x7f060002;
     }
     public static final class style {
+        public static final int AmandoTheme=0x7f070003;
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -74,10 +75,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f070007;
+        public static final int ListView=0x7f070004;
+        public static final int Schaltflaeche=0x7f070006;
+        public static final int SchaltflaecheText=0x7f070000;
+        public static final int Schaltfläche=0x7f070005;
+        public static final int TextStyleNormal=0x7f070001;
+        public static final int TextStyleUeberschrift=0x7f070002;
     }
 }
